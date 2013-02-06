7f689450c4ae6103d483fe3aca5207eb19710285
Maciej Pasternacki <maciej@pasternacki.net>
2013-02-06 13:36:37 +0100

    after-install: correctly call su and create directories

--- script/after-install.sh
+++ script/after-install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-install -d -o graphite -g graphite -m 0750 \
-    /opt/graphite/storage \
-    /opt/graphite/storage/log \
-    /opt/graphite/storage/log/webapp
+for d in storage storage/log storage/log/webapp ; do
+    test -d /opt/graphite/$d \
+        || install -d -o graphite -g graphite -m 0750 /opt/graphite/$d
+done
 
 cd /opt/graphite/service/graphite-web
-su graphite -c './manage syncdb --noinput'
+su graphite -s /bin/sh -c './manage syncdb --noinput'
